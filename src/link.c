e6dcca385085716c7fb05b2a49a519a83268ab68
hk122001 <hlibk@proton.me>
2024-05-14 15:38:11 -0600

    Add linking templates for rust functions

--- src/link.c
+++ src/link.c
@@ -1,5 +1,25 @@
 #include "link.h"
 
-int rust_function() {
-    return rust_function();
+int create_password_file() {
+    return create_password_file();
+}
+
+int read_header() {
+    return read_header();
+}
+
+int add_account() {
+    return add_account();
+}
+
+int modify_account() {
+    return modify_account();
+}
+
+int delete_account() {
+    return delete_account();
+}
+
+int read_message() {
+    return read_message();
 }
--- src/link.h
+++ src/link.h
@@ -1,6 +1,11 @@
 #ifndef LINK_H
 #define LINK_H
 
-int rust_function();
+int create_password_file();
+int read_header();
+int add_account();
+int modify_account();
+int delete_account();
+int read_message();
 
 #endif
