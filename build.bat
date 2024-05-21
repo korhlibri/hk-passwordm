86821dccfa77913fb3b8c5adf295eee3a42e0fb1
hk122001 <hlibk@proton.me>
2024-05-20 18:48:05 -0600

    Optimize compilation and remove verbose files

--- build.bat
+++ build.bat
@@ -1,6 +1,4 @@
 cargo build --lib --release
 
-gcc -c -o ./src/link.o ./src/link.c
-ar rcs ./target/release/libhk_passworm.a ./src/link.o
-
-go build -o app.exe -ldflags -H=windowsgui ./src/main.go
+@REM go build -o app ./src/main.go
+go build -o app -ldflags "-s -w" ./src/main.go
--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-# rm ./target/release/libhk_passwordm.a
-# rm ./src/link.o
 rm ./app
 
-cargo build --lib --release
+(cargo build --lib --release | exit 1)
 
-gcc -c -o ./src/link.o ./src/link.c
-# ar rcs ./target/release/libhk_passwordm.a ./src/link.o
-
-go build -o app ./src/main.go
+# go build -o app ./src/main.go
+go build -o app -ldflags "-s -w" ./src/main.go
